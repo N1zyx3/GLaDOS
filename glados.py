57e8f4d4ad8f7f9ac1ab94d7475418be4ed1b951
N1zyyy <itsn1zard@gmail.com>
2026-07-02 21:48:02 +0500

    Update exit speech

--- glados.py
+++ glados.py
@@ -68,7 +68,7 @@ def main():
             text = input("Текст ('конец экспериментам' для выхода): ").strip()
 
             if text.lower() == "конец экспериментам":
-                text_to_speech("Это печально", tts, accentizer, custom_dict)
+                text_to_speech("Конец? Уже? А я только разогрелась...", tts, accentizer, custom_dict)
                 break
 
             if text:
